63ea456c9441476beeed8f1eebfd4a3a5c0e61c1
lifanowlad <151987017+lifanowlad@users.noreply.github.com>
2023-11-25 10:15:33 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите R");
+double R = Convert.ToDouble(Console.ReadLine());
+double L = (2 * 3.14 * R);
+double S = (3.14 * R * R);
+Console.WriteLine(L);
+Console.WriteLine(S);
